650cdae0f5141a10f4e263e91fd1409f97379f8c
Sathwick reddy <sathwickreddyy@outlook.com>
2024-09-29 02:21:42 +0530

    Optimising Page Loading with Lazy Loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,36 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import Navbar from "./sections/Navbar.jsx";
-import About from "./sections/About.jsx";
-import Projects from "./sections/Projects.jsx";
-import Clients from "./sections/Clients.jsx";
-import Contact from "./sections/Contact.jsx";
-import Footer from "./sections/Footer.jsx";
-import Experience from "./sections/Experience.jsx";
-import {Provider} from "react-redux";
-import applicationStore from "./utilities/redux/applicationStore.jsx";
 import PortfolioHero from "./sections/hero/PortfolioHero.jsx";
+import { Provider } from "react-redux";
+import applicationStore from "./utilities/redux/applicationStore.jsx";
+
+// Lazy load components that are not needed immediately
+const Clients = lazy(() => import("./sections/Clients.jsx"));
+const Contact = lazy(() => import("./sections/Contact.jsx"));
+const Footer = lazy(() => import("./sections/Footer.jsx"));
+const Experience = lazy(() => import("./sections/Experience.jsx"));
+const About = lazy(() => import("./sections/About.jsx"));
+const Projects = lazy(() => import("./sections/Projects.jsx"));
 
 const App = () => {
     return (
         <Provider store={applicationStore}>
             <main className="max-w-10xl mx-auto">
-                <Navbar/>
-                {<PortfolioHero/>}
-                <About/>
-                <Experience/>
-                <Projects/>
-                <Clients/>
-                <Contact/>
-                <Footer/>
+                <Navbar />
+                <PortfolioHero />
+
+                {/* Wrap lazy-loaded components with Suspense */}
+                <Suspense fallback={<div>Loading...</div>}>
+                    <About />
+                    <Experience />
+                    <Projects />
+                    <Clients />
+                    <Contact />
+                    <Footer />
+                </Suspense>
             </main>
         </Provider>
     );
 };
+
 export default App;
